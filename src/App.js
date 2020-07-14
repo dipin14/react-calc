89f2dfbf2048f4ee4e21de2f7cfedd87628c7d03
Deepak Dinesh <deepakdinesh@Outlook.com>
2020-07-14 19:29:24 +0400

    Basic React App Structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <p>
+          Write calculator script code here.
+        </p>          
+    </div>
+  );
+}
+
+export default App;
